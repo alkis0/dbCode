942b7be2468ac32772e5f720be9cdb6465b1b8b9
alkis <alkisg1992@gmail.com>
2013-11-14 23:28:48 +0200

    basic code...

---
+++ src/DBconnect.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class DBconnect {
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+
+	public DBconnect(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost/test","root","password");
+			st=con.createStatement();
+			getData();
+		}catch(Exception e){
+			System.err.println(e);
+		}
+
+	}
+	
+	public void getData() throws Exception{
+		String query="select * from test1";
+		rs=st.executeQuery(query);
+		System.out.println("records from db:");
+		while(rs.next()){
+			String id=rs.getString("id");
+			String num=rs.getString("num");
+			System.out.println(id+" "+num);
+
+		}
+	}
+
+
+}
